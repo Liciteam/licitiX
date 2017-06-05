9cd488413effe98430377e825e469056223925bf
Cristian Iacob <ddpv.cristi@gmail.com>
2017-06-05 20:36:54 +0300

    administrare++

---
+++ tata.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+
+
+
+
+<html>
+    <head>
+        <link rel="icon" href="images/icon.png" />
+        <title>LicitiX.com | Auction is life</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Buy or sell all types of items in our online auction">
+        <meta name="keywords" content="auction site, auction online, buy, sell, auction shop">
+        <meta name="authors" content="Alexandru Rusu, Claudia Lucasi, Cristian Iacob">
+
+        <link rel="stylesheet" href="styles/admin.css" type="text/css"/>
+	</head>
+    <body>
+    
+    
+		
+	
+    	<div class='login-page'>
+		  	<div class='form'>
+			    <form class='login-form' action='poweruser.php' method='POST' >
+				      <input type='text' name = 'username' placeholder='username'>
+				      <input type='password' name = 'password' placeholder='password'>
+			      <button type='submit'>login</button>
+		   		</form>
+		  	</div>
+		</div>"
+	
+    </body>
+</html>
